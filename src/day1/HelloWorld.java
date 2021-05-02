2cb1c8b0edac9d634a9b4d026c3f2637e039ee3f
gayratishanov <82964907+gayratishanov@users.noreply.github.com>
2021-05-02 18:58:30 -0500

    added one more line of code " "

--- src/day1/HelloWorld.java
+++ src/day1/HelloWorld.java
@@ -7,5 +7,6 @@ public class HelloWorld {
         System.out.println("Hello World");
         System.out.println("Adding Github Now");
         System.out.println("one more commit");
+        System.out.println(" ");
     }
 }
